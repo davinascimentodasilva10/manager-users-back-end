666369fbba1cf06df580dbc3bdbf46f3bd0449fa
Davi Nascimento da Silva <davinascimentodasilva10@gmail.com>
2026-09-09 18:46:38 -0300

    Create Structure Initial of Project

---
+++ src/main/java/br/com/managerusers/ManagerUsersBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.managerusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerUsersBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerUsersBackEndApplication.class, args);
+	}
+
+}
